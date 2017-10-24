187db823c4f48cfe3a72ce07e15dde1157fdaf28
battlesushi <105021030@live.asia.edu.tw>
2017-10-24 12:07:41 +0800

    1024

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,11 +1,19 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class MainFrame extends JFrame {
-    public LoginFrame loginFrame=new LoginFrame();
-    public JMenuBar jmb=new JMenuBar();
-    public JMenu jmF=new JMenu("File");
+    private LoginFrame loginFrame=new LoginFrame();
+    private JMenuBar jmb=new JMenuBar();
+    private JMenu jmF=new JMenu("File");
+    private JMenuItem jMenuItemExit=new JMenuItem("Exit");
+    private JMenuItem jMenuItemLoto=new JMenuItem("Loto");
+    private JDesktopPane jdp=new JDesktopPane();//用不到cp的話
+    private JInternalFrame jInternalFrame=new JInternalFrame();
+
     public MainFrame(LoginFrame login){
         init();
         loginFrame=login;
@@ -14,9 +22,25 @@ public class MainFrame extends JFrame {
         this.setBounds(300,300,500,400);
         this.setLocationRelativeTo(null);
         this.setJMenuBar(jmb);
+        this.setContentPane(jdp);//用不到cp的話
         jmb.add(jmF);
-
-
+        jmF.add(jMenuItemExit);
+        jmF.add(jMenuItemLoto);
+        jInternalFrame.setBounds(0,0,200,80);
+        jMenuItemLoto.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jdp.add(jInternalFrame);
+                jInternalFrame.setVisible(true);
+            }
+        });
+        jMenuItemExit.setAccelerator(KeyStroke.getKeyStroke('X', Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
+        jMenuItemExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
 
         this.addWindowListener(new WindowAdapter() {
             @Override
